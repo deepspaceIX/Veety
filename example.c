1cb47fc588a376ede21c150a309099351c9e16eb
DS9 <93636816+deepspaceIX@users.noreply.github.com>
2022-09-17 19:59:14 -0700

    Create example.c

---
+++ example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "moros.c"
+
+int main() {
+  int i;
+  for (i = 0; i >= i; i++) {
+    startMoros();
+    if (workspace == 1) {
+      drawString("GRAPHING PROGRAM 200", 21, 11, 0);
+      button(25, 6, "GRAPH");
+    } else if (workspace == 2) {
+      drawString("GRAPHING PROGRAM", 21, 11, 0);
+    } else {
+      button(25, 6, "GRAPH");
+      drawString("Testing", 1, 1, 0);
+    }
+    window(50, 26, "Moros");
+    getInput();
+  }
+  return 0;
+}
