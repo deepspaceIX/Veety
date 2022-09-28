6e822e7873da11bf373db7451097bea85d303877
DS9 <93636816+deepspaceIX@users.noreply.github.com>
2022-09-27 21:46:52 -0700

    Update example.c

--- example.c
+++ example.c
@@ -5,17 +5,10 @@ int main() {
   while (0==0) {
     int workspace = getWorkspace();
     startMoros();
-    if (workspace == 1) {
-      drawString("GRAPHING PROGRAM 200", 21, 11, 0);
-      button(25, 6, "GRAPH");
-    } else if (workspace == 2) {
-      drawString("GRAPHING PROGRAM", 21, 11, 0);
-    } else {
-      button(25, 6, "GRAPH");
-      drawString("Testing", 1, 1, 0);
-      drawLine('y', slopeEYO, 0, 10);
-    }
-    window(50, 26, "main.c");
+    drawString("Graphing Calculator", 10, 1, 0);
+    button(15, 15, "graph");
+    //TextBox("test", 10, 10);
+    window(50, 26, "Ez-Graph 2");
     getInput();
   }
   return 0;
