560613435734c9861e96375f29b8308ab72a9694
DS9 <93636816+deepspaceIX@users.noreply.github.com>
2022-11-09 18:56:06 -0800

    Update example.c
    ///

--- example.c
+++ example.c
@@ -4,17 +4,13 @@
 int main(){
   while (0==0) {
     int workspace = getWorkspace();
-    startVeety();
+    startVeety(26, 26, "Graphing Calculator");
+    
     button(13, 10, "Graph");
     TextBox("Input Equation", 13, 6);
     TextBox("Secondary Input Test", 13, 20);
-    window(26, 26, "Graphing Calculator");
     
-    addTextBoxText("TEST1");
-    addTextBoxText("TEST2");
-    addTextBoxText("TEST3");
-    addTextBoxText("TEST4");
-
+    window();
     getInput();
   }
   return 0;
